3b1a4c4e584a5ab4093f68169521862eec2d71ea
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-10-18 22:33:12 +0100

    Deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "posts#index"
 end
