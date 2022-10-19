a0f011c52a472241e19c8666760bb5f785ff88dd
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-10-19 21:33:52 +0100

    Deviseで、アカウント名入力フォームを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+  devise_for :users, controllers: { registrations: 'users/registrations'}
+  get "users/show" => "users#show"
   get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "posts#index"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_18_205503) do
+ActiveRecord::Schema.define(version: 2022_10_19_194956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2022_10_18_205503) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
