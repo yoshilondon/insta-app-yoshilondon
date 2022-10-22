bb8b02fb5856b2b94614deaf518253c434ff3da2
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-10-22 21:54:34 +0100

    Post テーブルの追加とCSS設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,5 +9,6 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
   end
 end
