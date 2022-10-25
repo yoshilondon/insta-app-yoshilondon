50a3f71e43123b4e7b8317b54861ab61df719991
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-10-25 20:56:03 +0100

    コードの調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,9 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
+      devise_parameter_sanitizer.permit(:sign_in, keys: [:name])
+    end
 end
