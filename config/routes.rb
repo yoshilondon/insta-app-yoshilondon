e73cada9b6352036da902814a5c2fd1f6b4db21e
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-10-30 19:10:01 +0000

    profile画面のhtml作成とcss設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
   devise_for :users, controllers: { registrations: 'users/registrations'}
-  get 'users/show' => 'users#show'
-  get 'posts/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
 
