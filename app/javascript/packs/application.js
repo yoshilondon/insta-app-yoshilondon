57939f10f58ed315e6e4b0c8d13a41c61060d3a7
Yoshiki Norimatsu <yoyoyoyoshiki@hotmail.com>
2022-11-02 22:30:09 +0000

    JS実装①

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+require("trix")
+require("@rails/actiontext")
+
+document.addEventListener('DOMContentLoaded', () => {
+  window.alert('DOM LOADED')
+})
